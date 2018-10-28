dde165c03007c38a98d82dbc91f329ffaa08daa4
Joseph Oh <ohyjoseph@gmail.com>
2018-10-27 21:39:24 -0500

    Added to sliding window pattern

---
+++ patterns/slidingWindow/longestUniqueCharSubstring.js
@@ -0,0 +1,36 @@
+//Find the longest substrings where all chars are unique of a string
+//Note: Substring means the indices are contiguous
+
+//Time: O(n) Space: O(n)
+function longestUniqueCharSubstrings(string) {
+  let output = [];
+  let longestIndices = [0, 0];
+  const set = new Set(); //keeps track of used chars in constant time
+  let removeI = 0;
+  let addI = 0;
+  while (addI < string.length) { //instead of re-iterating every loop, just remove char if next one to be added is already in set otherwise add next char to set
+    let removeChar = string[removeI];
+    let addChar = string[addI];
+    if (addI === removeI || !set.has(addChar)) {
+      set.add(addChar);
+      let currentLength = addI - removeI + 1;
+      let longestLength = longestIndices[1] - longestIndices[0] + 1;
+      if (currentLength === longestLength) output.push([removeI, addI]);
+      else if (currentLength > longestLength) {
+        longestIndices = [removeI, addI];
+        output = [[removeI, addI]];
+      }
+      addI++;
+    } else {
+      set.delete(removeChar);
+      removeI++;
+    }
+  }
+  return output.map(([leftI, rightI]) => { //storing indices so mapping actual string results to return
+    return string.substring(leftI, rightI+1);
+  });
+}
+
+console.log(longestUniqueCharSubstrings('mississippi')); //['mis', 'sip']
+console.log(longestUniqueCharSubstrings('pp')); //['p', 'p']
+console.log(longestUniqueCharSubstrings('')); //[]
--- patterns/slidingWindow/sumExistsInSubArray.js
+++ patterns/slidingWindow/sumExistsInSubArray.js
@@ -5,27 +5,26 @@
 function sumExistsInSubArray(array, target) {
   let removeI = 0;
   let addI = 0;
-  let sum;
+  let sum = 0;
   while (addI <= array.length) { //instead of re-summing every loop, just remove what's not needed and add what is when necessary
     let removeValue = array[removeI];
     let addValue = array[addI];
-    if (addI === removeI) {
-      sum = addValue;
-      addI++;
-    }
-    else if (sum === target) {
-      return true;
-    }
-    else if (sum < target) { //increase window size
+    if (addI === removeI || sum < target) { //need first part of if because sum might equal 0 erroneously because no value has been added yet
       sum += addValue; //sum might equal NaN because addI could be out of index but doesn't matter because loop will end right after
       addI++;
-    } else { //decrease window size
+    }
+    else if (sum > target) { //decrease window size
       sum -= removeValue;
       removeI++;
+    }
+    else {
+      return true;
     }
   }
   return false;
 }
+console.log(sumExistsInSubArray([1,0,0,8], 0)); //true
 console.log(sumExistsInSubArray([4,0,5,5], 14)); //true
 console.log(sumExistsInSubArray([4,0,5,5], 9)); //true
 console.log(sumExistsInSubArray([6,1,5,2], 10)); //false
+console.log(sumExistsInSubArray([6,1,5,2], 0)); //false
