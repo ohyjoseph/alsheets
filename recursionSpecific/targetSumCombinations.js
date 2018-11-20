cd22a65918a436d54efbc864d44132e6f7dcc85e
Joseph Oh <ohyjoseph@gmail.com>
2018-11-20 00:04:44 -0600

    Added comments

--- recursionSpecific/targetSumCombinations.js
+++ recursionSpecific/targetSumCombinations.js
@@ -7,21 +7,22 @@ function targetSumCombinations(nums, target = 0, numsUsed = 3) {
     else return [];
   }
   if (numsUsed < 1) return null;
+
   const output = [];
-  if (nums.length < numsUsed) return output;
+  if (nums.length < numsUsed) return output; //can't have a valid combination if not enough nums exist
   let filteredNums = filterNumbers(nums, numsUsed);
-  filteredNums.sort();
-  const setput = new Set();
-  let tempSet = new Set();
-  let prevNums = [];
+  filteredNums.sort(); //sort so that when using nums as a key for map, the same use of numbers stringify to the same key
+  const setput = new Set(); //used to prevent duplicate entries
+  let tempSet = new Set(); //used to store possible needed values in the last loop
+  let prevNums = []; //need this to bring nums to further nested loops
   recurse();
   return output;
   function recurse(startIndex = 0, iteration = 1) {
-    for (let i = startIndex; i < filteredNums.length - numsUsed + iteration + 1; i++) {
+    for (let i = startIndex; i < filteredNums.length - numsUsed + iteration + 1; i++) { //enter when on the last loop of the iteration
       prevNums.push(filteredNums[i]);
       if (iteration >= numsUsed - 1) {
-        let needed = target - prevNums.reduce((total, num) => total + num, 0);
-        if (tempSet.has(needed)) {
+        let needed = target - prevNums.reduce((total, num) => total + num, 0); //the num needed to match the target
+        if (tempSet.has(needed)) { //check the set of previously stored nums of this iteration to see if it matches the needed num
           let numArray = [...prevNums, needed];
           stringNums = numArray.toString();
           if (!setput.has(stringNums)) {
@@ -30,16 +31,16 @@ function targetSumCombinations(nums, target = 0, numsUsed = 3) {
           }
         }
         tempSet.add(filteredNums[i]);
-        if (i === filteredNums.length - 1) tempSet.clear();
+        if (i === filteredNums.length - 1) tempSet.clear(); //clear set when this iteration is done
       } else {
-        recurse(i + 1, iteration + 1);
+        recurse(i + 1, iteration + 1); //keep making loops until reaching the last loop
       }
       prevNums.pop();
     }
   }
 }
 
-function filterNumbers(numbers, numsUsed) {
+function filterNumbers(numbers, numsUsed) { //only allows numsUsed amount of each number because more than that could never be used in a single combination
   const map = new Map();
   return numbers.filter((num) => {
     let shouldAdd = false;
